a22b016fdf412ae85bb7fe5982941edf7d2271aa
barra_brian <barra_brian@ghotmail.com>
2023-11-08 13:29:29 -0300

    new senna and dumont checkouts htmt

--- clk/offer/js/redirect.js
+++ clk/offer/js/redirect.js
@@ -1,17 +1,16 @@
-//
-// var geo_city = "";
-// var geo_countryCode = "";
-//
-//
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       geo_city = JSON.parse(this.response).YourFuckingCity;
-//       geo_countryCode = JSON.parse(this.response).YourFuckingCountryCode;
-//    }
-// };
-// xhttp.open("GET", "https://wtfismyip.com/json", true);
-// xhttp.send();
+var geo_city = "";
+var geo_countryCode = "";
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    geo_city = JSON.parse(this.response).YourFuckingCity;
+    geo_countryCode = JSON.parse(this.response).YourFuckingCountryCode;
+  }
+};
+xhttp.open("GET", "https://wtfismyip.com/json", true);
+xhttp.send();
 
 
 
@@ -21,8 +20,25 @@ var sck = new URL(document.location).searchParams.get("sck");
 
 function redirect() {
 
-  var pid = "S84803235P";
-  var off = "";
-  window.top.location.href = "https://pay.hotmart.com/" + pid +
-    "?checkoutMode=10&offDiscount=ONLY5SLOTSLEFT&sck=" + sck;
+  // var pid = "S84803235P";
+  // var off = "";
+  // location.href = "https://pay.hotmart.com/" + pid +
+  //   "?checkoutMode=10&offDiscount=ONLY5SLOTSLEFT&sck=" + sck;
+
+  // location.href = "https://www.checkout-ds24.com/product/524297/?ds24tr=" + sck;
+
+
+  if (geo_countryCode == 'US' || geo_countryCode == 'CA' || geo_countryCode ==
+    'AU' || geo_countryCode == 'NZ' || geo_countryCode == 'IN' ||
+    geo_countryCode == 'ZA' || geo_countryCode == 'MX') {
+    location.href =
+      "https://pay.hotmart.com/Q88210954P?checkoutMode=10&offDiscount=ONLY5SLOTSLEFT&sck=" +
+      sck;
+    //senna
+  } else {
+    //dumont
+    location.href =
+      "https://pay.hotmart.com/H88208936H?checkoutMode=10&offDiscount=ONLY5SLOTSLEFT&sck=" +
+      sck;
+  }
 }
