b130e08d67eac5ad5ce303ee90374ef0717c4898
barra_brian <barra_brian@ghotmail.com>
2024-06-13 23:46:44 -0300

    BSU offer page v1

---
+++ ultra/offer/js/accordion.js
@@ -0,0 +1,24 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    this.classList.toggle("accordion-selected");
+    // this.classList.toggle("from-violet-500");
+    // this.classList.toggle("via-pink-500");
+    // this.classList.toggle("to-yellow-300");
+    this.classList.toggle("shadow");
+
+    // console.log(this.firstChild.nextElementSibling);
+    this.firstChild.nextElementSibling.classList.toggle("rotate-180");
+
+    var panel = this.nextElementSibling;
+    // console.log(panel);
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
